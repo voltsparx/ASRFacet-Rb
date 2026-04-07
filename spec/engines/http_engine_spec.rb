84597d4c5f2532146c8aeb77a909f9df590085c6
Voltsparx⌁ <voltsparx@gmail.com>
2026-04-07 19:15:39 +0530

    Modifications

--- spec/engines/http_engine_spec.rb
+++ spec/engines/http_engine_spec.rb
@@ -1,4 +1,16 @@
-# Part of ASRFacet-Rb — authorized testing only
+# SPDX-License-Identifier: Proprietary
+#
+# ASRFacet-Rb: Attack Surface Reconnaissance Framework
+# Copyright (c) 2026 voltsparx
+#
+# Author: voltsparx
+# Repository: https://github.com/voltsparx/ASRFacet-Rb
+# Contact: voltsparx@gmail.com
+# License: See LICENSE file in the project root
+#
+# This file is part of ASRFacet-Rb and is subject to the terms
+# and conditions defined in the LICENSE file.
+
 require "spec_helper"
 
 RSpec.describe ASRFacet::Engines::HttpEngine do
